d7bd499473b7df569c001f09fc59eac2b246ee9c
Vladimir Ivanov <wkololo_4ever@mail.ru>
2023-08-17 16:20:48 +0200

    Reproduction

---
+++ Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+using System.Reflection;
+using Microsoft.Coyote.Runtime;
+
+namespace CoyoteTesting;
+
+public class Test
+{
+    [Microsoft.Coyote.SystematicTesting.Test]
+    public static void TestThreads()
+    {
+        var thread = new Thread(() =>
+        {
+            Console.WriteLine($"Inner Thread start");
+            var _ = new LockInStaticClass();
+            Console.WriteLine($"Inner Thread end");
+        });
+        thread.Start();
+
+        var _ = new LockInStaticClass();
+        
+        thread.Join();
+    }
+    
+    public class LockInStaticClass
+    {
+        static LockInStaticClass()
+        {
+            Console.WriteLine($"Static ctor start");
+            SchedulingPoint.Interleave();
+            Console.WriteLine($"Static ctor end");
+        }
+    }
+}
